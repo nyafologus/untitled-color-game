50135660eacf09239289867613803aecad0a599c
Szofi <nyafologus@gmail.com>
2019-11-17 00:53:22 +0100

    Display colors and names in ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
   render() {
     return (
-      <div className='ColorBox'>
+      <div className='ColorBox' style={{ background: this.props.background }}>
+        <span>{this.props.name}</span>
         {/* routes to the individual color page */}
         <span>MORE</span>
       </div>
--- src/Palette.js
+++ src/Palette.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
 
 export default class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map((color) => <ColorBox background={color.color} name={color.name} />);
     return (
       <div className='Palette'>
         <div>{/* Navbar goes here */}</div>
         {/* ColorBox Components go here*/}
-        <div className='Palette-colors'>COLOR BOXES</div>
+        <div className='Palette-colors'>{colorBoxes}</div>
         <div>{/* Footer here */}</div>
       </div>
     );
