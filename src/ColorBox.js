6f73aec272fcb943e4a8c92d2ad074153e4d65d4
Szofi <nyafologus@gmail.com>
2019-11-17 16:17:19 +0100

    Add overlay div and explanation

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -13,6 +13,11 @@ export default class ColorBox extends Component {
     return (
       <CopyToClipboard text={background}>
         <div className='ColorBox' style={{ background }}>
+          {/* Why do we need another div? ('copy-overlay) */}
+          {/* Why dont we just scale up ColorBox? */}
+          {/* We want to scale up the whole box, but we need to make a below div for that separately, because we only want to scale up the background and not the whole ColorBox with all of its contents. Since we want to have different content inside, we need another div.*/}
+          {/* TLDR: when we use scale to grow a div, it will grow all of the content inside of the div as well */}
+          <div className='copy-overlay' style={{ background }} />
           <div className='copy-container'>
             <div className='box-content'>
               <span>{name}</span>
