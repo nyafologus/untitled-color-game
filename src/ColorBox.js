13761af074895f8349ad762f3384b1e4839e8869
Szofi <nyafologus@gmail.com>
2019-11-17 12:40:36 +0100

    Center ColorBox Copy button

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -11,8 +11,8 @@ export default class ColorBox extends Component {
           <div className='box-content'>
             <span>{name}</span>
           </div>
+          <button className='copy-button'>Copy</button>
         </div>
-        <button className='copy-button'>Copy</button>
         {/* routes to the individual color page */}
         <span className='see-more'>MORE</span>
       </div>
