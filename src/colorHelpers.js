2404c68c2552de168eb084acb752a2832514034a
Szofi <nyafologus@gmail.com>
2019-11-18 13:26:54 +0100

    Implement getRange functionality

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -40,21 +40,53 @@ function generatePalette(starterPalette) {
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
-
-    // to fill our new colors object,
-    // let's loop over the levels array
-    // and add each one as a prop name
-    // then initialize each one as an empty array
-    // example:
-
     colors: {
-      // 50: [],
-      // 100: [],
-      // 200: []
-      // ...
+      // instead of hardcoding it, make it dynamic
     }
   };
 
+  // build a new color object that will contain each level
+  // and set them all to an empty array
+  for (let level of levels) {
+    newPalette.colors[level] = [];
+  }
+
+  // fill in empty arrays by looping over every color (starterPalette.colors)
+  // generate a scale with all those different colors
+  // take the lightest color and add it as the '50' value
+  // make sure to generate rgb and rgba as well
   // return new Palette
-  return newPalette;
+  for (let color of starterPalette.colors) return newPalette;
+}
+
+// create a range of colors (dark -> main color -> white)
+// takes in a main hexColor
+function getRange(hexColor) {
+  // define the end color and set it to white
+  const end = '#fff';
+
+  // return an array that will include 3 colors:
+  // a dark shade (generated),
+  // the input hexColor (= our color)
+  // and the end color (white)
+  return [
+    // The first color in the range will be the darkest.
+    // Using black would produce too many dark colors when using it inside the
+    // generateScale function, so instead we take the main color and darken it by 1.4.
+    // That way the start of range color will be dark enough
+    // but wont compromise half of the palette with too many dark shades.
+
+    // Wrap hexColor into chroma() contructor to turn it into a chroma color
+    // then darken it by 1.4 and get a hex value out of it
+    chroma(hexColor).darken(1.4).hex(),
+    hexColor,
+    end
+  ];
+}
+
+// generate a defined number of color variants based on the input color
+// takes in a main hexColor
+// and the number of colors to generate
+function generateScale(hexColor, numberOfColors) {
+  chroma.scale();
 }
