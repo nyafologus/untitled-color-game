f89d0e0d144403e3d985a340cb5faf1640d510d5
Szofi <nyafologus@gmail.com>
2019-11-18 14:47:34 +0100

    Implement generatePalette helper function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 export default class App extends Component {
   render() {
+    console.log(generatePalette(seedColors[2], 10));
     return (
       <div>
         {/* Updating chosen palette because I get bored after a while :> */}
