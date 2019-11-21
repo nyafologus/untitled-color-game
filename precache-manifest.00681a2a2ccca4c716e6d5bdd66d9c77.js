899f289b0f69a64632efb646eb73ca2932cb1110
Szofi <nyafologus@gmail.com>
2019-11-21 02:43:30 +0100

    Updates

---
+++ precache-manifest.00681a2a2ccca4c716e6d5bdd66d9c77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71f5206ddc1c97f3a97a04f85d3f2f2",
+    "url": "/untitled-color-game/index.html"
+  },
+  {
+    "revision": "3ab6ebf9c5452daee2ce",
+    "url": "/untitled-color-game/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "35e0390fd485b1988573",
+    "url": "/untitled-color-game/static/css/main.418f546a.chunk.css"
+  },
+  {
+    "revision": "3ab6ebf9c5452daee2ce",
+    "url": "/untitled-color-game/static/js/2.cec4a476.chunk.js"
+  },
+  {
+    "revision": "35e0390fd485b1988573",
+    "url": "/untitled-color-game/static/js/main.baa282e0.chunk.js"
+  },
+  {
+    "revision": "def868ea998e65f3291a",
+    "url": "/untitled-color-game/static/js/runtime-main.52cd0679.js"
+  }
+]);
